47a5a984833ff40eeb27b2ed8b40e6b8d67b84b2
Kami Karras <kamikarras@gmail.com>
2023-12-06 16:20:11 -0800

    url update

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { CharacterControls } from './characterControls';
 import { KeyDisplay } from './utils';
 import * as THREE from "three";
 import io from "socket.io-client";
-const socket = io.connect("https://diary-of-a-low-res-student-kamikarras.vercel.app/");
+const socket = io.connect("https://diary-of-a-low-res-student-kamikarras.vercel.app/api/index");
 
 const loader = new GLTFLoader();
 
